fcbfcbb18ca3d7275fa0671bd1c4d0c95e309ae8
Alexander <dev@salex.pro>
2019-04-21 18:20:49 +0300

    responsive photo page

--- src/js/photo.js
+++ src/js/photo.js
@@ -2,9 +2,25 @@ import Glide from '@glidejs/glide'
 
 const glide = new Glide('.glide', {
     type: 'carousel',
-    perView: 3,
+    perView: (window.outerWidth < 640) ? 1 : 3,
     focusAt: 'center',
     gap: 5
 });
 
 glide.mount();
+
+const reBuild = currWidth => {
+    if (currWidth < 1024 && currWidth >= 640 && !glide.disabled) {
+        glide.update({ type: 'slider' });
+        glide.disable();
+    } else if ((currWidth >= 1024 || currWidth < 640) && glide.disabled) {
+        glide.update({ type: 'carousel', perView: (window.outerWidth < 640) ? 1 : 3 });
+        glide.enable()
+    }
+}
+
+window.addEventListener('resize', () => {
+    reBuild(window.outerWidth);
+})
+
+reBuild(window.outerWidth);
